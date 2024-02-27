daf0630e42a8a2bbbeeeb4df7cfd221d7ddf472d
Tushar472000 <135508486+Tushar472000@users.noreply.github.com>
2024-02-27 10:03:48 +0530

    Update _app.tsx
    scrept removed in app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,20 +39,7 @@ export default function App({
         <PersistGate persistor={persistor}>
           <SessionProvider session={session}>
             <Layout>
-              <Script
-                async
-                defer
-                src='https://www.googletagmanager.com/gtag/js?id=G-H1CHYCNFQV'
-              />
-              {/*------------ Google analytics start ---------------- */}
-              <Script async defer id='google-analytics'>
-                {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-H1CHYCNFQV');
-              `}
-              </Script>
+             
 
               <Component {...pageProps} />
             </Layout>
