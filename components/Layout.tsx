689b11a3423d1bbc1ad5e1a308301e44543c3591
Tushar472000 <135508486+Tushar472000@users.noreply.github.com>
2024-02-27 10:17:25 +0530

    Update Layout.tsx
    --

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -49,10 +49,22 @@ export default function Layout({ children }: LayoutProps, req: NextRequest) {
             href='https://res.cloudinary.com/bold-pm/image/upload/BBD/BM-favicon.png'
             className='object-cover'
           />
-         
-       
-         
-         
+        <script async defer
+            type='application/ld+json'
+            dangerouslySetInnerHTML={addProductJsonLd()}
+            key='product-jsonld'
+          />
+          <link
+            rel='preload'
+            as='image'
+            href='https://res.cloudinary.com/bold-pm/image/upload/BBD/subscribe-bg.webp'
+          />
+          <script async defer
+            dangerouslySetInnerHTML={facebookScript()}
+          />
+          <noscript
+            dangerouslySetInnerHTML={noscriptfb()}
+          />
         </Head>
         <Header />
         <main className='min-h-screen pt-[66px] lg:pt-[100px]'>{children}</main>
