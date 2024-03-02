501006f3cd4bfc79629999b1c2cb5d3ac63f94d1
Tushar Patil <manjushakarpe1982@gmail.com>
2024-03-02 11:54:30 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,11 +39,7 @@ export default function App({
         <PersistGate persistor={persistor}>
           <SessionProvider session={session}>
             <Layout>
-              <Script
-                async
-                defer
-                src='https://www.googletagmanager.com/gtag/js?id=G-H1CHYCNFQV'
-              />
+             
               {/*------------ Google analytics start ---------------- */}
               <Script async defer id='google-analytics'>
                 {`
