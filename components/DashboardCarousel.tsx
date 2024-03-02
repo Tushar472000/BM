a5859cad5733ae67d4e300c80c884fd9b1dcef4c
Tushar Patil <manjushakarpe1982@gmail.com>
2024-03-02 10:46:25 +0530

    Update DashboardCarousel.tsx

--- components/DashboardCarousel.tsx
+++ components/DashboardCarousel.tsx
@@ -1,4 +1,4 @@
-import Slider from 'react-slick';
+// import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
@@ -9,15 +9,23 @@ import { useSelector } from 'react-redux';
 import { Suspense, useEffect, useMemo, useState } from 'react';
 import { selectUser } from '@/features/userSlice';
 import { DashboardCarouselProps } from '@/interfaces/propsinterfaces';
-
+import dynamic from 'next/dynamic';
+const Slider = dynamic(() => import('react-slick'), {
+  ssr: false // Only load on client-side
+});
 export default function DashboardCarousel({ images }: DashboardCarouselProps) {
   const [productName, setProductName] = useState('');
   const [vendorName, setVendorName] = useState('');
   const [customerId, setCustomerId] = useState(0);
+  const [hydrated, setHydrated] = useState(false);
   const user = useSelector(selectUser);
   useEffect(() => {
     setCustomerId(user.isLoggedin ? user.user.id : 0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    setTimeout(() => {
+      setHydrated(true);
+    }, 6000);
   }, []);
   const addProduct = async (imageForVenderId: any) => {
     try {
@@ -48,47 +56,63 @@ export default function DashboardCarousel({ images }: DashboardCarouselProps) {
       <Head>
         <link rel='preload' as='image' href={images[0].mobileImageurl}></link>
         <link rel='preload' as='image' href={images[0].imagePath}></link>
-        <link rel='preconnect' href={images[0].eventRedirectiveUrl}></link>
       </Head>
       <Suspense
         fallback={
           <section className='relative h-40 w-full bg-gray-400 sm:h-44 md:mt-2 md:h-40 lg:h-60 xl:h-80'></section>
         }
       >
-        <Slider {...settings}>
-          {images.map((image, index) => (
-            <section key={index} aria-hidden='true'>
-              <div className='relative flex h-40 w-full overflow-hidden rounded-lg sm:h-44 sm:object-cover md:mt-2 md:h-40 lg:h-60 xl:h-80 '>
-                <Link
-                  target='_blank'
-                  href={image.eventRedirectiveUrl}
-                  passHref
-                  prefetch={false}
-                  onClick={() => addProduct(image.imageForVenderId)}
-                >
-                  <Image
-                    className='hidden sm:block'
-                    fill
-                    src={image.imagePath}
-                    alt={image.imageName}
-                    priority={true}
-                    loading='eager' 
-                  />
+        {hydrated === true ? (
+          <Slider {...settings}>
+            {images.map((image, index) => (
+              <section key={index} aria-hidden='true'>
+                <div className='relative flex h-40 w-full overflow-hidden rounded-lg sm:h-44 sm:object-cover md:mt-2 md:h-40 lg:h-60 xl:h-80 '>
+                  <Link
+                    target='_blank'
+                    href={image.eventRedirectiveUrl}
+                    passHref
+                    prefetch={false}
+                    onClick={() => addProduct(image.imageForVenderId)}
+                  >
+                    <Image
+                      className='hidden sm:block'
+                      fill
+                      src={image.imagePath}
+                      alt={image.imageName}
+                    />
 
-                  <Image
-                    className='block sm:hidden'
-                    fill
-                    src={image.mobileImageurl}
-                    alt={image.imageName}
-                    priority={true}
-                    loading='eager' 
-                    quality={60}
-                  />
-                </Link>
-              </div>
-            </section>
-          ))}
-        </Slider>
+                    <Image
+                      className='block sm:hidden'
+                      fill
+                      src={image.mobileImageurl}
+                      alt={image.imageName}
+                      priority={true}
+                    />
+                  </Link>
+                </div>
+              </section>
+            ))}
+          </Slider>
+        ) : (
+          <div className='relative flex h-40 w-full overflow-hidden rounded-lg sm:h-44 sm:object-cover md:mt-2 md:h-40 lg:h-60 xl:h-80 '>
+            <Image
+              className='block sm:hidden'
+              fill
+              src={images[0].mobileImageurl}
+              alt={images[0].imageName}
+              priority={true}
+              loading='eager'
+            />
+            <Image
+              className='hidden sm:block'
+              fill
+              src={images[0].imagePath}
+              alt={images[0].imageName}
+              priority={true}
+              loading='eager'
+            />
+          </div>
+        )}
       </Suspense>
     </>
   );
