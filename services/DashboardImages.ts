e27aef538a179c602e7448bd400253568150dfd7
Tushar Patil <manjushakarpe1982@gmail.com>
2024-03-04 14:57:08 +0530

    Update DashboardImages.ts

--- services/DashboardImages.ts
+++ services/DashboardImages.ts
@@ -5,12 +5,12 @@ const DashboardImages = () => {
       imageName: 'Banner1',
       imageForVenderId: 1,
       imagePath:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/gold-bar-blast.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/silver-philharmonics-sale.webp',
       mobileImageurl:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/gold-bar-blast-mob.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/silver-philharmonics-sale-mob.webp',
       isStatic: false,
       eventRedirectiveUrl:
-        'https://www.apmex.com/category/90241/gold-bars?_campaign=02122024-category90241-priceplay-corebullion-homepagehero'
+        'https://www.apmex.com/category/24305/austrian-silver-philharmonics?f_onsale=true&page=1&_campaign=02262024-category24305-priceplay-corebullion-homepagehero'
     },
     {
       id: 3,
@@ -28,59 +28,59 @@ const DashboardImages = () => {
       imageName: 'Banner4',
       imageForVenderId: 30,
       imagePath:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-gold-bar-sale.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/britannia_liberty.webp',
       mobileImageurl:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-gold-bar-sale-mob.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/britannia_liberty-mob.webp',
       isStatic: false,
       eventRedirectiveUrl:
-        'https://www.jmbullion.com/1-oz-gold-bar/'
+        'https://www.herobullion.com/2024-britannia-liberty-gold-silver/'
     },
     {
       id: 5,
       imageName: 'Banner5',
       imageForVenderId: 10,
       imagePath:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-asahi-gold-bar.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/gold_silver-kangaroo.webp',
       mobileImageurl:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-asahi-gold-bar-mob.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/gold_silver-kangaroo-mob.webp',
       isStatic: false,
       eventRedirectiveUrl:
-        'https://www.providentmetals.com/1-oz-gold-bar-asahi.html'
+        'https://www.providentmetals.com/2024-australian-kangaroo-coins.html'
     },
     {
       id: 7,
       imageName: 'Banner7',
       imageForVenderId: 34,
       imagePath:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/2024-maple-leaf-platinum-silver_gold.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-silver-rounds-lowest-premium.webp',
       mobileImageurl:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/2024-maple-leaf-platinum-silver_gold-mob.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-silver-rounds-lowest-premium-mob.webp',
       isStatic: false,
       eventRedirectiveUrl:
-        'https://bullionexchanges.com/search?search=2024%20Maple%20Leaf%20Coin%20BU'
+        'https://bullionexchanges.com/buy-silver/rounds?category_id=584&perPage=72&currentPage=1'
     },
     {
       id: 8,
       imageName: 'Banner8',
       imageForVenderId: 4,
       imagePath:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-gold--coins.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/american-gold-eagles.webp',
       mobileImageurl:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-gold--coins-mob.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/american-gold-eagles-mob.webp',
       isStatic: false,
-      eventRedirectiveUrl: 'https://sdbullion.com/random-1-oz-gold-coins'
+      eventRedirectiveUrl: 'https://sdbullion.com/1-oz-american-gold-eagle-coin-random-year-bu'
     },
     {
       id: 9,
       imageName: 'Banner9',
       imageForVenderId: 12,
       imagePath:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-silver-kookaburra.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/germania-silver-coins.webp',
       mobileImageurl:
-        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/1-oz-silver-kookaburra-mob.webp',
+        'https://res.cloudinary.com/bullionmentor/image/upload/Banners/germania-silver-coins-mob.webp',
       isStatic: false,
       eventRedirectiveUrl:
-        'https://monumentmetals.com/2024-australia-1-oz-silver-kookaburra-bu.html'
+        'https://monumentmetals.com/deals/2024-germania.html?page=1'
     }
   ];
   return dashImagesJson;
